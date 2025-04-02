8fd1aa304b82ac945ac236a2844a6c4414bf43bb
UsmaanShafiq <usmanshafiq933@gmail.com>
2025-04-02 06:11:03 +0500

    horizontal slider and cards added with minor responsiveness

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,12 +1,44 @@
-$('.slider').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  centerMode: true,
-  arrows: false,
-  vertical: true,
-  verticalSwiping: true,
-  focusOnSelect: true
+$(document).ready(function(){
+  $('.slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      centerMode: true,
+      arrows: false,
+      vertical: true,
+      verticalSwiping: true,
+      focusOnSelect: true,
+      responsive: [
+          {
+              breakpoint: 1024, // When screen width is 1024px or less
+              settings: {
+                  vertical: false, // Switch to horizontal mode
+                  verticalSwiping: false,
+                  slidesToShow: 2, // Adjust number of slides
+                  slidesToScroll: 1
+              }
+          },
+          {
+              breakpoint: 768, // When screen width is 768px or less
+              settings: {
+                  vertical: false,
+                  verticalSwiping: false,
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+              }
+          }
+      ]
+  });
 });
 
+
+$(document).ready(function(){
+  $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true
+  });
+});
